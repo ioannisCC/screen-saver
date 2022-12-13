cfa0591a3eb5e11c8cfca9cce8b78ee292187a43
Ioan Croitor Catargiu <98465741+ioannisCC@users.noreply.github.com>
2022-12-14 01:25:14 +0200

    Add files via upload

---
+++ screen_saver_v2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace screen_saver_v2
+{
+    partial class ball
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ball));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Uighur", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(256, 147);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(281, 52);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.AutoEllipsis = true;
+            this.button1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(317, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 69);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "click";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 72);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "please enter an integer (number of balls)\r\n                    and click the butt" +
+    "on";
+            // 
+            // ball
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ball";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "screen saver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ screen_saver_v2/Form2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace screen_saver_v2
+{
+    public partial class Form2 : Form
+    {
+        Random angleR;
+        int angle;
+
+        public Form2()
+        {
+            angleR = new Random();
+            angle = angleR.Next(360);
+            InitializeComponent();
+            GraphicsPath shape = new GraphicsPath();
+            shape.AddEllipse(10, 30, 50, 50);
+            this.Region = new Region(shape);
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double radian = (angle * Math.PI) / 180;
+            Top -= (int)(10 * Math.Sin(radian));
+            Left += (int)(10 * Math.Cos(radian));
+
+            //bounce 
+            if (this.DesktopLocation.Y <= 0)
+            {
+                angle = 360 - angle;
+                radian = (angle * Math.PI) / 180;
+            }
+            if (this.DesktopLocation.X <= 0)
+            {
+                angle = 180 - angle;
+                radian = (angle * Math.PI) / 180;
+            }
+            if (this.DesktopLocation.X >= Screen.PrimaryScreen.Bounds.Width - 50)
+            {
+                angle = 180 - angle;
+                radian = (angle * Math.PI) / 180;
+            }
+
+            if (this.DesktopLocation.Y >= Screen.PrimaryScreen.Bounds.Height - 50)
+            {
+                angle = 360 - angle;
+                radian = (angle * Math.PI) / 180;
+
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Alt && e.Control)
+                Application.Exit();
+        }
+    }
+}
